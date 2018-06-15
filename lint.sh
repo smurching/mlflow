e9013c13c220364e31d514c6f7f66ef380eb552b
Sid Murching <sid.murching@databricks.com>
2018-06-15 11:31:42 -0700

    update linter

--- lint.sh
+++ lint.sh
@@ -5,6 +5,7 @@ set -e
 FWDIR="$(cd "`dirname $0`"; pwd)"
 cd "$FWDIR"
 
-prospector --profile "$FWDIR/prospector.yaml" -i "example"
+pylint mlflow
+# prospector --profile "$FWDIR/prospector.yaml" -i "example"
 
 rstcheck README.rst
