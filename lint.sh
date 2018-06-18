c04c868e0f2c8fa58ccdaf2d3bb8915e89c66470
Sid Murching <sid.murching@databricks.com>
2018-06-17 18:17:23 -0700

    Try printing some prospector debug output

--- lint.sh
+++ lint.sh
@@ -5,7 +5,7 @@ set -e
 FWDIR="$(cd "`dirname $0`"; pwd)"
 cd "$FWDIR"
 
-pylint mlflow
-# prospector --profile "$FWDIR/prospector.yaml" -i "example"
+# pylint mlflow --rcfile "$FWDIR/pylintrc"
+prospector --profile "$FWDIR/prospector.yaml" -i "example" --show-profile --include-tool-stdout
 
 rstcheck README.rst
