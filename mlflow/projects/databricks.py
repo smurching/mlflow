27620fdb089d7935a830ba67d5935658c571532b
Sid Murching <sid.murching@databricks.com>
2020-03-16 21:42:25 -0700

    Fix MLflow project execution on databricks

--- mlflow/projects/databricks.py
+++ mlflow/projects/databricks.py
@@ -178,7 +178,7 @@ class DatabricksJobRunner(object):
             # NB: We use <= on the version specifier to allow running projects on pre-release
             # versions, where we will select the most up-to-date mlflow version available.
             # Also note, that we escape this so '<' is not treated as a shell pipe.
-            "libraries": [{"pypi": {"package": "'mlflow<=%s'" % VERSION}}],
+            "libraries": [{"pypi": {"package": "mlflow<=%s" % VERSION}}],
         }
         run_submit_res = self._jobs_runs_submit(req_body_json)
         databricks_run_id = run_submit_res["run_id"]
