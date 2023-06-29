e860863f3b49b66c006c9ccad64ee6c78bc763c4
Sid Murching <sid.murching@databricks.com>
2023-06-29 15:08:19 -0700

    Benchmark model version creation
    Signed-off-by: Sid Murching <sid.murching@databricks.com>

--- mlflow/models/model.py
+++ mlflow/models/model.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import json
 import logging
+import time
 import warnings
 
 import yaml
@@ -562,8 +563,13 @@ class Model:
             local_path = tmp.path("model")
             run_id = mlflow.tracking.fluent._get_or_start_run().info.run_id
             mlflow_model = cls(artifact_path=artifact_path, run_id=run_id, metadata=metadata)
+            s = time.time()
             flavor.save_model(path=local_path, mlflow_model=mlflow_model, **kwargs)
+            print(f"Saving model took {time.time() - s} seconds")
+            s = time.time()
             mlflow.tracking.fluent.log_artifacts(local_path, mlflow_model.artifact_path)
+            print(f"Logging model artifacts to tracking took {time.time() - s} seconds")
+
             tracking_uri = _resolve_tracking_uri()
             if (
                 tracking_uri == "databricks" or get_uri_scheme(tracking_uri) == "databricks"
@@ -578,11 +584,13 @@ class Model:
                 _logger.debug("", exc_info=True)
             if registered_model_name is not None:
                 run_id = mlflow.tracking.fluent.active_run().info.run_id
+                s = time.time()
                 mlflow.register_model(
                     "runs:/{}/{}".format(run_id, mlflow_model.artifact_path),
                     registered_model_name,
                     await_registration_for=await_registration_for,
                 )
+                print(f"Registering model took {time.time() - s} seconds")
         return mlflow_model.get_model_info()
 
 
